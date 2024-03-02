56ecda644650d4c48c0000b9af838dc20877b5e6
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-03-02 19:29:27 +0700

    commit

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from 'body-parser';
 import cookieSession from "cookie-session";
-
+import { errorHandler, NotFoundError } from '@vtickets/common';
 
 const app = express();
 app.set("true proxy", true);
@@ -11,8 +11,11 @@ app.use(cookieSession({
     signed: false,
     secure: false, // secure: process.env.NODE_ENV !== 'test',
 }));
+
 app.all('*', async (req, res) => {
     throw new Error("");
 })
+
+app.use(errorHandler);
 
 export { app };
