081a4430483cd9b2cd4be825da990be552a83cb2
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-03-02 20:24:51 +0700

    eslintrc

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,2 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
+import Header from '../components/header';
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+    return (
+        <div>
+            <Header currentUser={currentUser}/>
+            <div className='cotainer'>
+                <Component currentUser={currentUser} {...pageProps} />
+            </div>
+        </div>
+    );
+}
