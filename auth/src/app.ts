bb2484c2dde6b213441cba95176b4211b6379b0a
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-03-02 20:22:44 +0700

    eslintrc

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from 'body-parser';
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from '@vtickets/common';
+import { errorHandler, NotFoundError } from '../../common/src';
 
 const app = express();
 app.set("true proxy", true);
