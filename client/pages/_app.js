502a18646bfd06dd27b80a945db5bc362e134371
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-03-02 20:29:22 +0700

    heade, _app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,10 +5,30 @@ import Header from '../components/header';
 const AppComponent = ({ Component, pageProps, currentUser }) => {
     return (
         <div>
-            <Header currentUser={currentUser}/>
+            <Header currentUser={currentUser} />
             <div className='cotainer'>
                 <Component currentUser={currentUser} {...pageProps} />
             </div>
         </div>
     );
 }
+
+AppComponent.getInitialProps = async (appContext) => {
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get('/api/users/currentUser');
+
+    let pageProps = {};
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(
+            appContext.ctx,
+            client,
+            data.currentUser
+        )
+    }
+    return {
+        pageProps,
+        ...data
+    };
+};
+
+export default AppComponent;
