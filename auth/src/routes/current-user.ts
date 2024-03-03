3b62b974e2291370fa20228297d316f29b4d8841
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-03-03 17:54:23 +0700

    current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
-import { currentUser } from "../../../common/src";
+import { currentUser, requireAuth } from "../../../common/src";
 
 const router = express.Router();
 
-router.get("/api/users/current-user", currentUser, (req, res) => {
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
+    console.log(`Current User request ${req}`);
+
     res.send({ currentUser: req.currentUser || null });
 });
 
-export { router as currentUserRouter }
+export { router as currentUserRouter };
