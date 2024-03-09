5f53de0904d718b4415c3c8d597322a5ce475423
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-03-09 16:50:09 +0700

    ticket created publisher

--- common/src/events/ticket-created-publisher.ts
+++ common/src/events/ticket-created-publisher.ts
@@ -1,3 +1,7 @@
-import {Publisher} from "./base-publisher";
+import { Publisher } from "./base-publisher";
 import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+    subject: Subjects.TicketCreated = Subjects.TicketCreated;
+}
