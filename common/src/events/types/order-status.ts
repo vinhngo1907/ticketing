9332d017c24410e2ced05ac8edf1a1824dd77088
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-03-09 20:25:57 +0700

    common

---
+++ common/src/events/types/order-status.ts
@@ -0,0 +1,16 @@
+export enum orderStatus {
+    // when the order has been created, but 
+    // ticket it is trying to order has not been reserved
+
+    Created = 'created',
+
+    // The ticket the order is trying to reserve has already
+    // been reserved, or when the user has cancelled the order.
+    // The order expires before payment
+
+    Cancelled = 'cancelled',
+
+    // The order has reserved the ticket and the user has
+    // provided payment successfully
+    Complete = 'complete',
+}
